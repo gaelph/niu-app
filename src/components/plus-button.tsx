32d98551cb5cbada55d43ad0093aa3105337332a
gaelph <contact@gael-philippe.fr>
2019-11-17 15:54:00 +0100

    add plus button

---
+++ src/components/plus-button.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import { View, StyleSheet } from 'react-native'
+import { Feather } from '@expo/vector-icons'
+import IconButton from './icon-button'
+
+import Colors from '../theme/colors'
+import Dimensions from '../theme/dimensions'
+
+export default function PlusButton({ onPress }) {
+  return (
+    <View style={styles.container}>
+      <View style={styles.content}>
+        <IconButton name="plus" size={32} color='white' provider={Feather} onPress={onPress} />
+      </View>
+    </View>
+  )
+} 
+
+const styles = StyleSheet.create({
+  container: {
+    position: 'absolute',
+    bottom: 12,
+    left: 0, right: 0,
+    height: Dimensions.appBar.height,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  content: {
+    backgroundColor: Colors.text.primary,
+    width: 48, 
+    height: 48,
+    borderRadius: 24,
+    justifyContent: 'center',
+    alignItems: 'center',
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 1,
+    },
+    shadowOpacity: 0.07,
+    shadowRadius: 3.84,
+
+    elevation: 2,
+  }
+})
