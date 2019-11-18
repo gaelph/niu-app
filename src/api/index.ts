b1d29832de020b7b223296b8d55f78aa9de0278c
gaelph <contact@gael-philippe.fr>
2019-11-18 09:38:24 +0100

    preftetch from store

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,6 +1,8 @@
+import { useReducer, useEffect, useState, useCallback, useRef } from 'react'
+import { AsyncStorage } from 'react-native'
 import { reducer, ApiActions, Action } from "./api-reducer"
+import uuid from 'uuid/v4'
 export * from './queries'
-import { useReducer, useEffect, useState, useCallback } from 'react'
 import { CursoredList } from './types'
 
 interface PaginatedQuery {
@@ -8,16 +10,55 @@ interface PaginatedQuery {
   pageSize?: number
 }
 
-function callApi<TYPE, PARAMS extends PaginatedQuery>(query: ApiQuery<TYPE, PARAMS>, params: PARAMS, dispatch: React.Dispatch<Action<TYPE | Error>>) {
-  dispatch({ type: ApiActions.Fetching })
+function transformDates(object) {
+  if (typeof object === 'string') {
+    if (/\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}/.test(object)) {
+      return new Date(object)
+    }
+  }
 
-  query(params)
-  .then(response => {
-    dispatch({ type: ApiActions.Fetched, payload: response })
+  if (Array.isArray(object)) {
+    return object.map(transformDates)
+  }
+
+  if (typeof object === 'object' && object !== null && object !== undefined) {
+    return Object.entries(object).reduce((acc, [key, value]) => {
+      acc[key] = transformDates(value)
+
+      return acc
+    }, {})
+  }
+
+  return object
+} 
+
+function callApi<TYPE, PARAMS extends PaginatedQuery>(queryId: string, query: ApiQuery<TYPE, PARAMS>, params: PARAMS, dispatch: React.Dispatch<Action<TYPE | Error | boolean>>) {
+  dispatch({ type: ApiActions.Fetching, payload: true })
+
+  AsyncStorage.getItem(`Query_${queryId}`, (err, result) => {
+    if (err) {
+      console.warn(err)
+    }
+
+    if (result) {
+      let transformed = transformDates(JSON.parse(result))
+      // console.log('Serving result from storage for query', queryId, transformed)
+      // dispatch({ type: ApiActions.Fetched, payload: transformed })
+    }
+
+    query(params)
+    .then(response => {
+      dispatch({ type: ApiActions.Fetching, payload: false })
+      dispatch({ type: ApiActions.Fetched, payload: response })
+  
+      AsyncStorage.setItem(`Query_${queryId}`, JSON.stringify(response))
+    })
+    .catch(error => {
+      dispatch({ type: ApiActions.Error, payload: error })
+    })
   })
-  .catch(error => {
-    dispatch({ type: ApiActions.Error, payload: error })
-  })
+
+
 }
 
 type ApiQuery<TYPE, PARAMS extends PaginatedQuery> = (params?: PARAMS) => Promise<TYPE>
@@ -34,10 +75,11 @@ export function useApiPolling<TYPE, PARAMS>(query: ApiQuery<TYPE, PARAMS>, inter
   let [state, dispatch] = useReducer(...reducer<TYPE>())
   let [t, setT] = useState<number>(undefined);
   let [call, setCall] = useState<boolean>(false);
+  let queryId = useRef(query.name)
 
   useEffect(() => {
-    callApi<TYPE, PARAMS>(query, params, dispatch)
-  }, [call]);
+    callApi<TYPE, PARAMS>(queryId.current, query, params, dispatch)
+  }, [call, dispatch]);
 
   useEffect(() => {
     let i = setTimeout(() => {
@@ -50,8 +92,9 @@ export function useApiPolling<TYPE, PARAMS>(query: ApiQuery<TYPE, PARAMS>, inter
   }, [call])
 
   const refresh = useCallback(() => {
-    callApi<TYPE, PARAMS>(query, params, dispatch)
-  }, [call])
+    console.log('refresh', state)
+    callApi<TYPE, PARAMS>(queryId.current, query, params, dispatch)
+  }, [query, params, state, dispatch, call])
 
   return {
     data: state.data as TYPE, 
@@ -64,13 +107,14 @@ export function useApiPolling<TYPE, PARAMS>(query: ApiQuery<TYPE, PARAMS>, inter
 
 export function useApi<TYPE, PARAMS extends PaginatedQuery>(query: ApiQuery<TYPE, PARAMS>, params?: PARAMS): ApiHookData<TYPE> {
   let [state, dispatch] = useReducer(...reducer<TYPE>())
+  let queryId = useRef(uuid())
 
   useEffect(() => {
-    callApi<TYPE, PARAMS>(query, params, dispatch)
+    callApi<TYPE, PARAMS>(queryId.current, query, params, dispatch)
   }, [])
 
   const refresh = useCallback(() => {
-    callApi<TYPE, PARAMS>(query, params, dispatch)
+    callApi<TYPE, PARAMS>(queryId.current, query, params, dispatch)
   }, [])
 
   let cursoredList = state.data as CursoredList<TYPE>
@@ -78,7 +122,7 @@ export function useApi<TYPE, PARAMS extends PaginatedQuery>(query: ApiQuery<TYPE
   let fetchMore = () => {}
   if (cursoredList != null && cursoredList.items && cursoredList.cursor) {
     fetchMore = () => {
-      callApi<TYPE, PARAMS>(query, cursoredList.cursor as PARAMS, dispatch)
+      callApi<TYPE, PARAMS>(queryId.current, query, cursoredList.cursor as PARAMS, dispatch)
     }
   }
 
