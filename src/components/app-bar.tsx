41ba228ab575a0eb6a91471ebb868fc37036b048
gaelph <contact@gael-philippe.fr>
2019-11-28 21:37:14 +0100

    add version dialog

--- src/components/app-bar.tsx
+++ src/components/app-bar.tsx
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from 'react';
-import { View, StyleSheet, Dimensions as Dim } from 'react-native'
+import React, { useCallback } from 'react';
+import { View, StyleSheet, Dimensions as Dim, Alert } from 'react-native'
 import { Feather } from '@expo/vector-icons'
 
 import IconButton from './icon-button'
@@ -8,12 +8,24 @@ import Colors from '../theme/colors'
 import Dimensions from '../theme/dimensions'
 import { useDimensions } from '../support'
 
+const packagJson = require('../../package.json')
+
 export default function AppBar() {
   let Screen = useDimensions('window')
 
+  const showVersionDialog = useCallback(() => {
+    Alert.alert(
+      'Niu',
+      `version ${packagJson.version}`,
+      [
+        {text: 'OK'}
+      ]
+    )
+  }, [])
+
   return (
     <View style={[styles.container, { width: Screen.width }]}>
-      <IconButton name="settings" size={24} color={Colors.foreground} provider={Feather}  />
+      <IconButton name="settings" size={24} color={Colors.foreground} provider={Feather} onPress={showVersionDialog}  />
     </View>
   )
 }
